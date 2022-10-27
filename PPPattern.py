fb80a53f8e050064877a8e92210cd576ca4f9fe5
TejaswiniSruthi <82491774+TejaswiniSruthi@users.noreply.github.com>
2022-10-27 23:04:53 +0530

    Create PPPattern.py

---
+++ PPPattern.py
@@ -0,0 +1,10 @@
+#pg 5 sub:981
+
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if(j==i or j==n-i-1):
+            print(n-i,end=' ')
+        else:
+            print(' ',end=' ')
+    print()
